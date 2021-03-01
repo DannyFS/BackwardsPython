7f65a6cfd922c188e110961d02823e7e00a760ab
DannyS <47923594+DannyFS@users.noreply.github.com>
2021-03-01 18:58:03 -0500

    Add files via upload

---
+++ compile.py
@@ -0,0 +1,19 @@
+import os, sys
+
+def main():
+    filePath = os.getcwd()
+    fileName = sys.argv[1]
+
+    if fileName[-4:] != ".bpy":
+        return
+
+    sourceContent = open(fileName, 'r').read()
+
+    fileContent = ""
+    for x in range(len(sourceContent)):
+        fileContent += sourceContent[-x - 1]
+
+    exec(fileContent)
+
+if __name__ == "__main__":
+    main()
---
+++ toReverse.py
@@ -0,0 +1,22 @@
+import os, sys
+
+def main():
+    filePath = os.getcwd()
+    fileName = sys.argv[1]
+
+    if fileName[-3:] != ".py":
+        return
+    os.rename(fileName, fileName[:-2] + "bpy")
+    fileName = fileName[:-2] + "bpy"
+
+    sourceContent = open(fileName, 'r').read()
+
+    fileContent = ""
+    for x in range(len(sourceContent)):
+        fileContent += sourceContent[-x - 1]
+
+    with open(fileName, 'w') as file:
+        file.write(fileContent)
+
+if __name__ == "__main__":
+    main()
